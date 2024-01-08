902e9a86f437046ab4445f6f3ed9eb7fd7e264ae
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-01-08 16:49:01 -0500

    creates Category model

--- models/Category.js
+++ models/Category.js
@@ -1,13 +1,28 @@
+// DEPENDENCIES:
+// require sequelize library
 const { Model, DataTypes } = require('sequelize');
-
+// require db connection
 const sequelize = require('../config/connection.js');
 
+// Creating Category model:
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    // define types, rules for each column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
+  
   {
     sequelize,
     timestamps: false,
