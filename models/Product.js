c08859f4c0901d49ebe1c4a8b1a13425d2ab4b81
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-01-08 16:58:31 -0500

    adds Product model

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,50 @@
+// DEPENDENCIES:
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
+// Creating Product model:
 class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    // define types, rules for each column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
