e108be7a8878d1ff4193bbe5fe9d7e7c79457c1f
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-01-08 17:08:01 -0500

    adds Tag model

--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,33 @@
-const { Model, DataTypes } = require('sequelize');
+// DEPENDENCIES:
+// import important parts of sequelize library
+const { Model, DataTypes } = require("sequelize");
+// import our database connection from config.js
+const sequelize = require("../config/connection.js");
 
-const sequelize = require('../config/connection.js');
-
+// Initialize Tag model (table) by extending off Sequelize's Model class
+// Creating Tag model:
 class Tag extends Model {}
-
+// set up fields and rules for Tag model
 Tag.init(
   {
     // define columns
+    // define types, rules for each column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
